ff9704094e1fddf0548ab9a9ae85e5938112360e
carlosak97 <carlos-a-k97@hotmail.com>
2022-05-15 17:28:33 +0300

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,36 @@
+# import time
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    deploy_mocks,
+    get_account,
+    LOCAL_BLOCKCHAIN_ENVIRENMENTS,
+)
+
+
+def deploy_fund_me():
+    account = get_account()
+    # pass the price feed address to our fundMe contract
+
+    # if we are on a persistent network like rinkeby, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRENMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    # time.sleep(1)
+    print(f"Contract deployed to {fund_me.address}")
+    # for test
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,26 @@
+from brownie import accounts, MockV3Aggregator, network, config
+
+
+FORKED_LOCAL_ENVIRENMENTS = ["mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRENMENTS = ["development", "ganache-local"]
+
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+
+
+def get_account():
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRENMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRENMENTS
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
+    print("Mocks Deployed!")
